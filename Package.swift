4b55dd89984d85917fde92110edac2fb5633dc31
Antonio J Pallares <antonio.pallares@thingthing.co>
2022-07-22 14:16:21 +0200

    Update package dependencies to latest versions

--- Package.swift
+++ Package.swift
@@ -14,27 +14,32 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/FleksySDK/FleksyEngine-iOS",
-            exact: "3.5.15"
+            exact: "3.5.18"
         ),
+        .package(
+            url: "https://github.com/FleksySDK/iOS-CoreServices",
+            exact: "1.0.0"
+        )
     ],
     targets: [
         .target(
             name: "FleksySDK",
             dependencies: [
                 .product(name: "FleksyEngine", package: "FleksyEngine-iOS"),
+                .product(name: "iOSCoreServices", package: "iOS-CoreServices"),
                 "FleksyApps",
                 "FleksyKeyboardSDK"
             ]
         ),
         .binaryTarget(
             name: "FleksyApps",
-            url: "https://spm.fleksy.com/FleksyKeyboardSDK/v4.1.0/FleksyApps.xcframework.zip",
-            checksum: "12e266d3834d185b60c0b68c88d4b85cae945e3f9055b758fa1bda94083401d9"
+            url: "https://spm.fleksy.com/FleksyKeyboardSDK/v4.1.2/FleksyApps.xcframework.zip",
+            checksum: "e7be9f77b974f2a914f10756ca61d50df78bad9a719e76bbb08d88409d062585"
         ),
         .binaryTarget(
             name: "FleksyKeyboardSDK",
-            url: "https://spm.fleksy.com/FleksyKeyboardSDK/v4.1.0/FleksyKeyboardSDK.xcframework.zip",
-            checksum: "a6c3412c3306a9bcfb0c620e1a498f007aa0bafa4006141fd83e79fb9f0e8d8f"
+            url: "https://spm.fleksy.com/FleksyKeyboardSDK/v4.1.2/FleksyKeyboardSDK.xcframework.zip",
+            checksum: "d607a043185cdb6fad1b5e8febafb6406db0395cb0d0c1ba1580c3432d505923"
         ),
     ]
 )
