b247b387375c7938ceff9d4b45714b6c3922d332
Fleksy CI <developers@thingthing.co>
2022-11-28 14:35:57 +0000

    Update package dependencies to latest versions

--- Package.swift
+++ Package.swift
@@ -14,11 +14,11 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/FleksySDK/FleksyEngine-iOS",
-            exact: "3.5.30"
+            exact: "3.6.0"
         ),
         .package(
             url: "https://github.com/FleksySDK/iOS-CoreServices",
-            exact: "1.3.0"
+            exact: "1.3.2"
         )
     ],
     targets: [
@@ -32,8 +32,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "FleksyKeyboardSDK",
-            url: "https://spm.fleksy.com/FleksyKeyboardSDK/v4.2.5/FleksyKeyboardSDK.xcframework.zip",
-            checksum: "d3978bf2c89212187b0f744bb32590a3537c8984cd23232c2ed138cd099d5821"
+            url: "https://spm.fleksy.com/FleksyKeyboardSDK/v4.3.3/FleksyKeyboardSDK.xcframework.zip",
+            checksum: "84a37da1add1b8a01fccfaec170f156ca3bdb1ee9d6dc011b50109e9b3fe3a2c"
         ),
     ]
 )
