0f408550031751b7568ff8a7c20c89a80ac6ab15
Fleksy CI <developers@thingthing.co>
2023-11-08 09:28:15 +0000

    Update package dependencies to latest versions

--- Package.swift
+++ Package.swift
@@ -37,8 +37,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "FleksyKeyboardSDK",
-            url: "https://spm.fleksy.com/FleksyKeyboardSDK/v4.17.0/FleksyKeyboardSDK.xcframework.zip",
-            checksum: "c8790b97f35a6cd0c4d66bc9ff520448263f3f69de0d5096f14776703b6279e2"
+            url: "https://spm.fleksy.com/FleksyKeyboardSDK/v4.17.1/FleksyKeyboardSDK.xcframework.zip",
+            checksum: "6c6da5506c6aa5839574704cbc99f3c9a2e99422698dd41f6e1b0eb70f4dbe35"
         ),
     ]
 )
